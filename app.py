a22d082e9661589fd643084968cd253977fc5dde
kotianshivani <31771602+kotianshivani@users.noreply.github.com>
2025-04-01 15:04:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
-# from langchain_community.vectorstores import Chroma
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_openai import ChatOpenAI
@@ -17,15 +13,14 @@ from langchain_core.output_parsers import StrOutputParser
 from base64 import b64decode
 from dotenv import load_dotenv
 import os
-# from langchain_groq import ChatGroq
-from langchain_groq.chat_models import ChatGroq
-
-# from langchain.vectorstores import FAISS
-from langchain_community.vectorstores import FAISS
+from langchain_groq import ChatGroq
+from langchain.vectorstores import FAISS
 from langchain.docstore.document import Document
 import pickle
-#from langchain_groq import ChatGroq
-# ChatGroq.model_rebuild()
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 # Initialize session state variables
 if 'cache_hits' not in st.session_state:
